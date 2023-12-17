6a67611a0216b275663c91506e46dc3f527cf37f
ryu <jjonak.ryu20@gmail.com>
2023-12-18 01:08:11 +0900

    reform main/end menu

--- Assets/scipts/End.cs
+++ Assets/scipts/End.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter(Collider collision)
     {
         if (collision.tag == "Player")
         {
